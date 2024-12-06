555ccd170e3fd607073d093c5c24a2d3bbe9adb2
sapat <rkamble@sapat.com>
2024-12-07 01:52:13 +0530

    Revert "initial commit"
    This reverts commit e9f6190f4b779e820bc231609f694fe606f46118.

--- lib/services/api_url.dart
+++
@@ -1,5 +0,0 @@
-/// api urls
-class AppUrl{
-  static String apiKey = 'REZiSXUMdOKO67Un7YamOA==Ig8LdUej0D2NQ9jk';
-  static String Baseurl = 'https://api.api-ninjas.com/v1/weather';
-}
--- lib/utils/common_method/convert_temp.dart
+++
@@ -1,9 +0,0 @@
-/// to convert the temperature
-String convertTemperature(double temperature, bool isCelsius ) {
-  if (isCelsius) {
-    return temperature.toStringAsFixed(1);
-  } else {
-    double fahrenheit = (temperature * 1.8) + 32;
-    return fahrenheit.toStringAsFixed(1);
-  }
-}
